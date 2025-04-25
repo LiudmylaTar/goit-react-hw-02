a92fda0cca7d930d54e563cfeb5f66b6ace994ce
Liudmyla_Taran <taran.lyudok@gmail.com>
2025-04-25 13:32:19 +0300

    HW-2 finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,63 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import Feedback from './components/Feedback/Feedback';
 import Options from './components/Options/Options';
+import Notification from './components/Notification/Notification';
 
 import './App.css';
+import { createWebSocketModuleRunnerTransport } from 'vite/module-runner';
 
 function App() {
-  const [count, setCount] = useState(0);
+  const storedVotes = JSON.parse(localStorage.getItem('votesCount'));
+
+  const [voutesCount, setVoutesCount] = useState(
+    storedVotes || {
+      good: 0,
+      neutral: 0,
+      bad: 0,
+    },
+  );
+
+  useEffect(() => {
+    localStorage.setItem('votesCount', JSON.stringify(voutesCount));
+  }, [voutesCount]);
+
+  const updateFeedback = (feedbackType) => {
+    setVoutesCount((prevState) => ({
+      ...prevState,
+      [feedbackType]: prevState[feedbackType] + 1,
+    }));
+  };
+
+  const totalFeedback =
+    voutesCount.good + voutesCount.neutral + voutesCount.bad;
+  const positiveFeedback = Math.round((voutesCount.good / totalFeedback) * 100);
+
+  const resetFeedback = () => {
+    setVoutesCount({
+      good: 0,
+      neutral: 0,
+      bad: 0,
+    });
+  };
 
   return (
-    <>
-      <h1>Sip Happens Café</h1>
-      <p>
+    <div className="container">
+      <h1 className="main_header">Sip Happens Café</h1>
+      <p className="text">
         Please leave your feedback about our service by selecting one of the
         options below.
       </p>
-      <Options />
-      <Feedback />
-    </>
+      <Options
+        onLeaveFeedback={updateFeedback}
+        onReset={resetFeedback}
+        totalFeedback={totalFeedback}
+      />
+      {totalFeedback === 0 ? (
+        <Notification />
+      ) : (
+        <Feedback votes={voutesCount} positive={positiveFeedback} />
+      )}
+    </div>
   );
 }
 
--- src/components/Feedback/Feedback.jsx
+++ src/components/Feedback/Feedback.jsx
@@ -1,10 +1,11 @@
-import ccs from './Feedback.module.css';
-export default function Feedback() {
+import css from './Feedback.module.css';
+export default function Feedback({ votes, positive }) {
   return (
-    <ul className={CSS.list}>
-      <li>Good</li>
-      <li>Neutral</li>
-      <li>Bad</li>
+    <ul className={css.list}>
+      <li>Good: {votes.good}</li>
+      <li>Neutral: {votes.neutral}</li>
+      <li>Bad: {votes.bad}</li>
+      <li className={css.positive}> Positive: {positive}%</li>
     </ul>
   );
 }
